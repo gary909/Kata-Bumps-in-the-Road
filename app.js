f1acc00ceb67251764708633f47ec4767ec5ea0d
gary909 <garywhite61@hotmail.com>
2023-06-23 11:21:51 +0200

    code

---
+++ app.js
@@ -0,0 +1,9 @@
+function bump(x){
+
+}
+
+console.log(bump("n")); // "Woohoo!"
+console.log(bump("__nn_nnnn__n_n___n____nn__nnn")); // "Woohoo!"
+console.log(bump("nnn_n__n_n___nnnnn___n__nnn__")); // "Woohoo!"
+console.log(bump("_nnnnnnn_n__n______nn__nn_nnn")); // "Car Dead"
+console.log(bump("______n___n_")); // "Woohoo!"
